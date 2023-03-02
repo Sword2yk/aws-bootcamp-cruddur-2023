9045fae9654c9adea2c096365784b04e26799877
Chinedu <obichinedu@gmail.com>
2023-03-02 17:56:46 +0100

    CloudWatch Logs Implementation.

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -5,13 +5,14 @@ tracer = trace.get_tracer("home.activities")
 
 class HomeActivities:
   def run():
+    #logger.info("HomeActivities")
     with tracer.start_as_current_span("home-activites-mock-data"):
       span = trace.get_current_span()
       now = datetime.now(timezone.utc).astimezone()
       span.set_attribute("app.now", now.isoformat())
       results = [{
         'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
-        'handle':  'Chinedu Obi',
+        'handle':  'Andrew Brown',
         'message': 'Cloud is very fun!',
         'created_at': (now - timedelta(days=2)).isoformat(),
         'expires_at': (now + timedelta(days=5)).isoformat(),
