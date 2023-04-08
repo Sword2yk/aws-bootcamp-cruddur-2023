43dc175dc2cc9c6d0c489d72345b1d44da294665
Chinedu <obichinedu@gmail.com>
2023-04-08 11:54:52 +0000

    resolved seed.sql 500 error

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,15 +1,15 @@
 -- this file was manually created
 INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
-  ('Andrew Brown', 'user@exmaple.com', 'andrewbrown' ,'MOCK'),
-  ('Andrew Bayko', 'users@exmaple.com', 'bayko' ,'MOCK'),
-  ('Londo Mollari','lmollari@centari.com' ,'londo' ,'MOCK'),
-  ('Chinedu Obi','hobichinedu@gmail.com' ,'Chinedu Obi' ,'Ulake');
+  ('Chinedu Obi','hobichinedu@gmail.com','Ulake','35a420f9-1124-4ba8-9497-d9771798b858'),
+  ('Andrew Bayko', 'users@exmaple.com', 'bayko','MOCK'),
+  ('Londo Mollari','lmollari@centari.com' ,'londo' ,'MOCK');
+
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
   (
-    (SELECT uuid from public.users WHERE users.handle = 'andrewbrown' LIMIT 1),
+    (SELECT uuid from public.users WHERE users.handle = 'Ulake' LIMIT 1),
     'This was imported as seed data!',
     current_timestamp + interval '10 day'
   )
