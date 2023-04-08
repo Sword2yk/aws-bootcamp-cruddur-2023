fcde959ed23176f999e5e386dc2d2a96b28e0f70
Chinedu <obichinedu@gmail.com>
2023-04-08 15:57:32 +0000

    testing

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -2,8 +2,8 @@
 INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
   ('Chinedu Obi','hobichinedu@gmail.com','Ulake','35a420f9-1124-4ba8-9497-d9771798b858'),
-  ('Andrew Bayko', 'users@exmaple.com', 'bayko','f73f4b05-a59e-468b-8a29-a1c39e7a2222'),
-  ('Londo Mollari','lmollari@centari.com' ,'londo' ,'MOCK');
+  ('Andrew Bayko', 'users@exmaple.com', 'bayko','f73f4b05-a59e-468b-8a29-a1c39e7a2222');
+  
 
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
