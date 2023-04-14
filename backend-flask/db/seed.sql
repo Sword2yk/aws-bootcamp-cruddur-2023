69b81838c35493763b96fc4ec6b70e5aa0581ae5
Chinedu <obichinedu@gmail.com>
2023-04-14 18:11:45 +0000

    ddb fixing error

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,6 +1,7 @@
 -- this file was manually created
 INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
+  ('Andrew Brown', 'obichinedu@gmail.com', 'andrewbrown', 'MOCK'),
   ('Chinedu Obi','hobichinedu@gmail.com','Ulake','35a420f9-1124-4ba8-9497-d9771798b858'),
   ('Andrew Bayko', 'bayko@exampro.co', 'bayko','f73f4b05-a59e-468b-8a29-a1c39e7a2222');
   
@@ -9,7 +10,7 @@ VALUES
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
   (
-    (SELECT uuid from public.users WHERE users.handle = 'Ulake' LIMIT 1),
+    (SELECT uuid from public.users WHERE users.handle = 'andrewbrown' LIMIT 1),
     'This was imported as seed data!',
     current_timestamp + interval '10 day'
   )
